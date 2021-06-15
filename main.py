1b9b9fd789ce659eeaaf76f74c1083a847d5f807
Nathan Abraham <abnathan123@gmail.com>
2021-06-15 11:22:32 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import csv_utils
 
+
+def startup():
+    print("Welcome to the command line data processor!")
+
+    # User will type in a number, and the corresponding task will be executed
+    print("1: Retrieve csv file from url")
+    print("2: Drop row or columns from data")
+
+
 test_url = "http://winterolympicsmedals.com/medals.csv"
-csv_utils.get_csv()
 
-
+if __name__ == "__main__":
+    choice = int(
+        input("Type in the number corresponding to the task you want to do: "))
+    if choice == 1:
+        csv_utils.get_csv()
+    if choice == 2:
+        pass
+    if choice == 3:
+        pass
