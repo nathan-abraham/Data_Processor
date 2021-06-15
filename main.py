a176eb9fcea53a8833df8f416937c37bf6c656b7
Nathan Abraham <abnathan123@gmail.com>
2021-06-15 11:27:47 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ def startup():
     # User will type in a number, and the corresponding task will be executed
     print("1: Retrieve csv file from url")
     print("2: Drop row or columns from data")
+    print("5: Exit the program")
 
 
 test_url = "http://winterolympicsmedals.com/medals.csv"
 
 if __name__ == "__main__":
-    choice = int(
-        input("Type in the number corresponding to the task you want to do: "))
-    if choice == 1:
-        csv_utils.get_csv()
-    if choice == 2:
-        pass
-    if choice == 3:
-        pass
+    while True:
+        startup()
+        choice = int(input("Type in the number corresponding to the task you want to do: "))
+        if choice == 1:
+            csv_utils.get_csv()
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 5:
+            break
+    
+    
