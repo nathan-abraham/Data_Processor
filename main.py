f5b5cb3b22a6ff2fc7e2d1a110ceb2814e49d968
Nathan Abraham <abnathan123@gmail.com>
2021-06-16 12:47:38 -0400

    Updated error handling

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import csv_utils
 import data_processor
 import console_utils
 
-
+colorama.init(autoreset=True)
 test_url = "http://winterolympicsmedals.com/medals.csv"
 
 if __name__ == "__main__":
@@ -15,8 +15,10 @@ if __name__ == "__main__":
     console_utils.startup()
 
     # Initializing dataframe variable
-    df = None
-    csv_csv_loaded = False
+    df = pd.DataFrame()
+    csv_loaded = False
+
+    # The function sleep(n) causes a delay for n seconds    
     while True:
         console_utils.print_options()  # Print options
         choice = console_utils.choose()  # Get input from user
@@ -27,19 +29,28 @@ if __name__ == "__main__":
             file_name = csv_utils.get_csv()
 
             # Create dataframe from csv
-            if df != None:
+            if file_name != None:
                 df = pd.read_csv(f"{file_name}.csv", error_bad_lines=False)
                 csv_loaded = True
+                console_utils.print_success("Dataframe succesfully loaded.")
+            sleep(1)
         elif csv_loaded and choice == 2:
             data_processor.drop_data(df)
         elif csv_loaded and choice == 3:
-            pass
+            data_processor.get_file_name()
+        elif choice == 4:
+            if df.empty:
+                console_utils.print_error("Load in a csv file first!")
+            else:
+                file_name = data_processor.get_file_name()
+                data_processor.save_csv(df, file_name=file_name)
+            sleep(1)
         elif choice == 5:
             break
         elif choice > 5:
-            print("Enter a number between 1 and 5")
+            console_utils.print_error("Enter a number between 1 and 5")
             sleep(1)
         else:
-            print("Load in a csv file first!")
+            console_utils.print_error("Load in a csv file first!")
             sleep(1)
         console_utils.print_seperator()
