22c958678f1cb2a9c0b84a5b73690a1a7beee9dc
Nathan Abraham <abnathan123@gmail.com>
2021-06-17 13:47:25 -0400

    Added progress bars

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import colorama
 import pandas as pd
 from time import sleep
 from colorama import Fore, Back, Style
+from rich import console
 
 import csv_utils
 import data_processor
@@ -9,6 +10,7 @@ import console_utils
 
 colorama.init(autoreset=True)
 test_url = "http://winterolympicsmedals.com/medals.csv"
+test2_url = "https://raw.githubusercontent.com/nathan-abraham/Safety-Data-Analysis/main/datasets/air_quality_index_india.csv"
 
 if __name__ == "__main__":
     # Load startup
@@ -32,23 +34,26 @@ if __name__ == "__main__":
             if file_name != None:
                 df = pd.read_csv(f"{file_name}.csv", error_bad_lines=False)
                 csv_loaded = True
-                console_utils.print_success("Dataframe succesfully loaded.")
+                console_utils.dummy_progress_bar("Loading dataframe...")
             sleep(1)
         elif csv_loaded and choice == 2:
             data_processor.drop_data(df)
         elif csv_loaded and choice == 3:
             data_processor.graph_data(df)
-        elif choice == 4:
+        elif csv_loaded and choice == 4:
+            data_processor.remove_outliers(df)
+            sleep(1)
+        elif choice == 5:
             if df.empty:
                 console_utils.print_error("Load in a csv file first!")
             else:
                 file_name = data_processor.get_file_name()
                 data_processor.save_csv(df, file_name=file_name)
             sleep(1)
-        elif choice == 5:
+        elif choice == 6:
             break
-        elif choice > 5:
-            console_utils.print_error("Enter a number between 1 and 5")
+        elif choice > 6:
+            console_utils.print_error("Enter a number between 1 and 6")
             sleep(1)
         else:
             console_utils.print_error("Load in a csv file first!")
